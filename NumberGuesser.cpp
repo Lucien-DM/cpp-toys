d5e0b4e16285c054cf339b2ed2497db85f1dfb83
Lucien-DM <lucien@nuggetcomputersinc.net>
2026-06-13 18:43:25 +1000

    Completed NumberGuesser.cpp

--- NumberGuesser.cpp
+++ NumberGuesser.cpp
@@ -2,17 +2,26 @@
 #include <unistd.h>
 
 int main() {
-	const int NUMBER = 12
+	const int NUMBER = 12;
 	std::cout << "Hey, see if you can guess this number!\n";
+	int Guess;
 	while (true) {
 		std::cout << "Take a guess...\n";
-		int Guess;
 		std::cin >> Guess;
-		if (Guess == NUMBER) {
+		if (std::cin.fail()) {
+			std::cout << "That's not a number!\n";
+			std::cin.clear();
+			std::cin.ignore(999, '\n');
+		} else if (Guess == NUMBER) {
 			std::cout << "That's it!\n";
 			return 0;
 		} else if (Guess > NUMBER) {
 			std::cout << "My number is lower\n";
 		} else if (Guess < NUMBER) {
 			std::cout << "My number is higher\n";
-		} //TODO
+		} else {
+			std::cout << "error\n";
+			return 1;
+		}
+	}
+}
